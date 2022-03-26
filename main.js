dd914efeca6209e71ed3392c6af6b3d8745ead43
Duyen Nguyen <hongduyen0705@gmail.com>
2022-03-26 10:50:44 +0200

    made functions for open, close mobile nav, close nav when link clicks and window resize

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
 // Queries
 const burgerIcon = document.querySelector(".burger-icon");
-console.log(burgerIcon);
-
 const nav = document.querySelector("nav");
+const navLinks = nav.querySelectorAll("ul a");
 
-// Toggle nav show
+// open mobile nav
+function openMobileNav() {
+  nav.classList.add("open");
+  burgerIcon.setAttribute("src", "./images/icon-close.svg");
+}
 
+// Close mobile nav
+function closeMobileNav() {
+  nav.classList.remove("open");
+  burgerIcon.setAttribute("src", "./images/icon-hamburger.svg");
+}
+
+// Toggle open/close mobile nav
 burgerIcon.addEventListener("click", (e) => {
-  console.log("clicked");
-  if (nav.classList.contains("show")) {
-    nav.classList.remove("show");
-    burgerIcon.setAttribute("src", "./images/icon-hamburger.svg");
+  if (nav.classList.contains("open")) {
+    closeMobileNav();
   } else {
-    nav.classList.add("show");
-    burgerIcon.setAttribute("src", "./images/icon-close.svg");
+    openMobileNav();
+  }
+});
+
+// Close nav when nav link clicks
+navLinks.forEach((link) => {
+  link.addEventListener("click", closeMobileNav);
+});
+
+// Reset to burger icon if widtch >= 1024px
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1024 && nav.classList.contains("open")) {
+    nav.classList.remove("open");
+    burgerIcon.setAttribute("src", "./images/icon-hamburger.svg");
   }
 });
