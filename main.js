e7988aac27d94a971efaa847d021c7229f748698
Duyen-codes <hongduyen0705@gmail.com>
2022-10-26 11:31:09 +0300

    remove comment block

--- main.js
+++ main.js
@@ -43,7 +43,6 @@ window.addEventListener("scroll", closeMobileNav);
 
 // Back to top
 toTopButton.addEventListener("click", () => {
-  // window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   window.scroll(0, 0);
 });
 
